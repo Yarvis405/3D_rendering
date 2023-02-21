99b26d0b563a440543f7eaba1e6e835905edf1bf
graveyard <368271@guhsd.net>
2023-02-21 06:04:44 +0000

    Initial commit

--- script/raycasting.js
+++ script/raycasting.js
@@ -1,3 +1,33 @@
 //DOM
 
 let canvas, ctx;
+canvas = document.querySelector("#lienzo");
+ctx = canvas.getContext("2d");
+
+const resize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+
+const clearScreen = (color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+const mapObj = {};
+const playerObj = {};
+
+class Map {
+  constructor() {}
+}
+
+class player {
+  constructor() {}
+}
+
+(function loop() {
+  resize();
+  clearScreen();
+
+  requestAnimationFrame(loop);
+})();
